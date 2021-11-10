1a35773ba1fa2bdbc8fa366494ec5cc2bdba28b2
RubyCodesYT <81465414+RubyCodesYT@users.noreply.github.com>
2021-11-11 12:13:17 +1300

    Update UIUtils.java

--- src/main/java/aura/ui/UIUtils.java
+++ src/main/java/aura/ui/UIUtils.java
@@ -1,5 +1,10 @@
 package aura.ui;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.dsl.FXGL;
 
 public class UIUtils {
-
+   public static void drawUIComponent(int x, int y, UIComponent u) {
+     String name = u.toString();
+     Entity e = FXGL.entityBuilder().at(x, y).view(name).buildAndAttach();
+   }
 }
